c57698ef49ca841c5c23186c2c776bddb695b485
burcukeskin <keskinnburcuu@gmail.com>
2021-07-29 12:16:51 +0300

    Laravel Kategori, Ödeme, Sipariş ve Kulanıcı sayfaları oluşturuldu.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login_form()
+    {
+        return view('user.login');
+    }
+    public function register_form()
+    {
+        return view('user.register');
+    }
+
+}
---
+++ resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+
+        <a class="navbar-brand" href="#"><img src="/img/logo.png"></a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuac">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menuac">
+            <form class="navbar-form">
+                <div class="input-group">
+                    <input type="text" id="navbar-search" class="form-control" placeholder="Research">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-default  my-2 my-sm-0">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </form>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Basket <span class="badge badge-theme">5</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" active href="#">Register</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Profil
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Orders</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
